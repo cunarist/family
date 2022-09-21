1f352f1217bc384a33ae4e08278b206b65a7cf56
temeddix <temeddix@gmail.com>
2022-09-21 14:28:12 +0900

    Improve folder structure

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 import bpy  # type:ignore
 
+from operators import DuplicateHierarchy, DuplicateHierarchyLinked, DeleteHierarchy
+
 bl_info = {
     "name": "Family",
     "author": "Cunarist",
@@ -9,97 +11,6 @@ bl_info = {
     "category": "Object",
     "doc_url": "https://cunarist.com/family",
 }
-
-
-class DuplicateHierarchy(bpy.types.Operator):
-    # Use this as a tooltip for menu items and buttons.
-    """Duplicate selected objects including their children"""
-
-    # Unique identifier for buttons and menu items to reference.
-    bl_idname = "object.duplicate_hierarchy"
-    # Display name in the interface.
-    bl_label = "Duplicate Hierarchy"
-    # Enable undo for the operator.
-    bl_options = {"REGISTER", "UNDO"}
-
-    # execute() is called when running the operator.
-    def execute(self, context):
-
-        # The original script
-        selected_objects = context.selected_objects
-        targets = []
-        for selected_object in selected_objects:
-            all_children = selected_object.children_recursive
-            targets.append(selected_object)
-            targets += all_children
-        for target in targets:
-            target.select_set(True)
-
-        bpy.ops.object.duplicate(linked=False)
-
-        # Lets Blender know the operator finished successfully.
-        return {"FINISHED"}
-
-
-class DuplicateHierarchyLinked(bpy.types.Operator):
-    # Use this as a tooltip for menu items and buttons.
-    """Duplicate linked selected objects including their children"""
-
-    # Unique identifier for buttons and menu items to reference.
-    bl_idname = "object.duplicate_hierarchy_linked"
-    # Display name in the interface.
-    bl_label = "Duplicate Hierarchy Linked"
-    # Enable undo for the operator.
-    bl_options = {"REGISTER", "UNDO"}
-
-    # execute() is called when running the operator.
-    def execute(self, context):
-
-        # The original script
-        selected_objects = context.selected_objects
-        targets = []
-        for selected_object in selected_objects:
-            all_children = selected_object.children_recursive
-            targets.append(selected_object)
-            targets += all_children
-        for target in targets:
-            target.select_set(True)
-
-        bpy.ops.object.duplicate(linked=True)
-
-        # Lets Blender know the operator finished successfully.
-        return {"FINISHED"}
-
-
-class DeleteHierarchy(bpy.types.Operator):
-    # Use this as a tooltip for menu items and buttons.
-    """Delete selected objects including their children"""
-
-    # Unique identifier for buttons and menu items to reference.
-    bl_idname = "object.delete"
-    # Display name in the interface.
-    bl_label = "Delete Hierarchy"
-    # Enable undo for the operator.
-    bl_options = {"REGISTER", "UNDO"}
-
-    # execute() is called when running the operator.
-    def execute(self, context):
-
-        # The original script
-        selected_objects = context.selected_objects
-        targets = []
-        for selected_object in selected_objects:
-            all_children = selected_object.children_recursive
-            targets.append(selected_object)
-            targets += all_children
-        for target in targets:
-            bpy.data.objects.remove(target, do_unlink=True)
-
-        # Lets Blender know the operator finished successfully.
-        return {"FINISHED"}
-
-    def invoke(self, context, event):
-        return context.window_manager.invoke_confirm(self, event)
 
 
 def register():
