5015998098ec9e9d297e14c20327324dad8c353c
temeddix <temeddix@gmail.com>
2022-12-22 01:19:28 +0900

    Version 3.2

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ from .property_groups import FamilySettings
 bl_info = {
     "name": "Family",
     "author": "Cunarist",
-    "version": (3, 1),
+    "version": (3, 2),
     "blender": (3, 0, 0),
     "description": "An addon for Blender with select, duplicate and delete operations in hierarchy",
     "category": "Object",
