0cf66f92e11cd0e99640ef539fbf12de21eed794
Donghyun Kim <temeddix@gmail.com>
2024-07-19 15:58:21 +0900

    Set minimum Blender version to 4

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "name": "Family",
     "author": "Cunarist <cunarist@gmail.com>",
     "version": (3, 3, 1),
-    "blender": (3, 0, 0),
+    "blender": (4, 0, 0),
     "description": "An addon for Blender with select,"
     " duplicate and delete operations in hierarchy",
     "category": "Object",
